6abcadfd803b5e37bf7983cb3eaf3d079111c27a
nikola-wd <nikola_wd@hotmail.com>
2023-08-22 13:33:26 +0200

    failed to instll tailwind. Good enough for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/App.jsx', './src/components/**/*.jsx'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
